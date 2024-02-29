fc0fe2a46feb139575789055d8ab690cb2ed12b5
ezFazio <personajescond@gmail.com>
2024-02-28 22:50:46 -0300

    feat(structure): added first sections

---
+++ src/components/profile.jsx
@@ -0,0 +1,12 @@
+import styles from "./profile.module.css"
+
+
+const Profile = () => {
+    return (
+      <div className={styles.profile}>
+        <h1>Acá irá el profile</h1>
+      </div>
+    )
+  }
+  
+  export default Profile;
